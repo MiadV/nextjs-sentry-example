0db670c30b71b309eb4fb3db0d32d271f7669a1a
Miad Vosoughi <miadsoft@gmail.com>
2024-03-04 15:30:34 +0800

    Update DataDog configuration

--- components/DataDog.tsx
+++ components/DataDog.tsx
@@ -1,23 +1,15 @@
-import { datadogRum } from '@datadog/browser-rum';
+import { datadogLogs } from '@datadog/browser-logs';
 
-datadogRum.init({
-  applicationId: '31266d63-0517-440e-98a4-fc263c457d5d',
+datadogLogs.init({
   clientToken: 'pub8e10ce0bba84a7c73fe3aa64d27a745f',
   site: 'us5.datadoghq.com',
   service: 'test-app',
   env: 'production',
   // Specify a version number to identify the deployed version of your application in Datadog
-  // version: '1.0.0',
-  sessionSampleRate: 20,
-  sessionReplaySampleRate: 0,
-  trackUserInteractions: false,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: 'mask-user-input',
-  // Specify URLs to propagate trace headers for connection between RUM and backend trace
-  allowedTracingUrls: [
-    { match: 'https://example.com/api/', propagatorTypes: ['tracecontext'] },
-  ],
+  version: 'test-1.0.0',
+  sessionSampleRate: 100,
+  forwardErrorsToLogs: true,
+  forwardConsoleLogs: 'all',
 });
 
 export default function DatadogInit() {
